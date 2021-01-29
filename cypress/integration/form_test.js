8f7446d3ce1ebdeb12c4a374ca0f948e91f66aea
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-29 16:58:53 -0500

    cypress tests are passed

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,48 @@
+describe("PizzaOrderForm app", () => {
+    beforeEach(() => {
+        //arbitrary code you want running before your tests start
+        cy.visit('http://localhost:3000/pizza');
+    });
+    it('Try name input', () => {
+        cy.get('input[name="name"]')
+          .should("have.value", "")
+          .type("Jalpa Shah")
+          .should("have.value","Jalpa Shah");
+    });
+    it('lets select size', () => {
+        cy.get('select')
+          .select('large')
+          .should("have.value", "large");
+    });
+    it('let us select sauce',() => {
+        cy.get('[type="radio"]')
+          .check('bbqSauce');
+    });
+    it('let us select some toppings', () => {
+        cy.get('input[name="onion"]')
+        .check()
+        .should('be.checked')
+    })
+    it('let select gluten free', () => {
+        cy.get('input[name="glutenFree"]')
+          .check()
+          .should('be.checked')
+    })
+    it('should disabled the submit button', () => {
+        cy.get('input[name="name"]')
+          .should("have.value", "")
+          .type("Jalpa Shah")
+          .should("have.value","Jalpa Shah");
+
+        cy.get('select')
+          .select('large')
+          .should("have.value", "large");
+
+        cy.get('[type="radio"]')
+          .check('bbqSauce');
+
+        cy.get('button')
+          .click()
+        
+    })
+})
